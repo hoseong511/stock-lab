c92c9c655c08959e862790bb09d7837a5f1826bc
hoseong511 <thdghtjd115@naver.com>
2021-02-11 23:27:11 +0900

    update test_mongodb_handler.py 0.1

--- tests/test_mongodb_handler.py
+++ tests/test_mongodb_handler.py
@@ -27,3 +27,18 @@ class MongoDBHandlerTestCase(unittest.TestCase):
         self.mongodb._client["stocklab_test"]["corp_info"].insert_many(docs)
 
     def test_insert_item(self):
+        print(inspect.stack()[0][3])
+        doc = { "item": "SamSung Card", "related": "SamSung", "qty": 25,
+               "tags": ["green", "red"], "account": [10, 11] }
+        _id = self.mongodb.insert_item(doc, "stocklab_test", "corp_info")
+        assert _id
+        print(_id)
+
+    def test_insert_items(self):
+        print(inspect.stack()[0][3])
+        docs = [ {"item": "LG", "related": "LG", "qty": 25, "tags": ["red"], "account": [14, 21]},
+                 {"item": "LG 화학", "related": "LG", "qty": 25, "tags": ["green", "red"], "account": [14, 21]}
+        ]
+        ids = self.mongodb.insrt_items(docs, "stocklab_test", "corp_info")
+        assert ids
+        print(ids)
